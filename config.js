899abea886255717dc65dfdfcddc296e38183a46
hieulight98 <144131911+hieulight98@users.noreply.github.com>
2023-09-05 20:49:00 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "Gửi người thương",
+    introTitle: 'Trần Thùy Dương',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    btnIntro: '^^Okii^^',
+    title: 'Đôi ta cùng cố gắng ĐỒNG GIẢI cao nhá 🥰',
+    desc: 'NHỚ RẰNG TỚ LUÔN VÌ CẬU ĐƯỢC KHÔNG ',
     btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    btnNo: 'Mơ à :3',
+    question: 'Vậy mình cùng nhau cố gắng nhá <3',
+    btnReply: 'Chả vậy thì sao <3',
+    reply: 'Nhưng trước hết phải luôn giữ gìn sức khỏe nha <33333333',
+    mess: 'Mình biết mà 🥰. IU nhiều nhiều 😘😘',
+    messDesc: 'Giờ ra cổng mình đưa cái này đi!',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+    messLink: 'https://www.facebook.com/' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
